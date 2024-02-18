30937473460743564e42f7d51b17a3edd865898a
Phunsup Suwannasophon <avg.artvengers@gmail.com>
2024-02-18 13:20:02 +0700

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Phunsup S. on 17/2/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
